bfdb84fc6219bb54eb043a753727856529a03ec5
Seb1776 <sebastianwho1776@gmail.com>
2021-11-29 18:59:43 -0500

    Tried to fix 5

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
     {
 		float number = 3.14159f;
-		Console.WriteLine("Float: {0}", number.ToString("0.##").Replace(',', '.'));
+		Console.WriteLine("Float: {0}", number.ToString("F2").Replace(',', '.'));
     }
 }
