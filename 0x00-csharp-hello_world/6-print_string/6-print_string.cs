cde9f7a09812fe57d00202c3275429f65c9ed6ba
Seb1776 <sebastianwho1776@gmail.com>
2021-11-29 15:24:16 +0000

    Added project 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine($"{string.Concat(Enumerable.Repeat(str, 3))}\n{str.Substring(0, 9)}");
+    }
+}
