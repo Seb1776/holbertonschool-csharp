cdb0fd54dcd803131b9e1c3101b010cefc7a266c
Seb1776 <sebastianwho1776@gmail.com>
2021-11-29 19:07:27 -0500

    Fix 9

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -9,7 +9,7 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0}", string.Format("{0:0.00%}", percent));
-        Console.WriteLine("Currency: {0}", currency.ToString("C2"));
+        Console.WriteLine("Currency: {0}", currency.ToString("C2").Replace(" ", string.Empty));
 	}
 }
 
