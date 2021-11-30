4769444ff2363087f037585e89d74d655574ff55
Seb1776 <sebastianwho1776@gmail.com>
2021-11-30 01:37:51 -0500

    Added project 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.Write("\n");
+        
+        else
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    if (j == i)
+                        Console.Write(((char)92).ToString());
+                    else
+                        Console.Write(" ");
+            
+                Console.Write("\n");
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
