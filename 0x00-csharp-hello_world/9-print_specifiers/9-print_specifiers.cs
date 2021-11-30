5ec6c687c50cd9bfd1f3a3ff7cbb30b7be9a18ed
Seb1776 <sebastianwho1776@gmail.com>
2021-11-29 19:20:04 -0500

    Fixed 9

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Globalization;
 
 class Program
 {
 	static void Main(string[] args)
 	{
-        CultureInfo cI = new CultureInfo("en-US", false);
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0}", string.Format("{0:0.00%}", percent));
-        Console.WriteLine("Currency: {0}", currency.ToString("C2").Replace(" ", string.Empty));
+		Console.WriteLine("Percent: {0}\nCurrency: {1}", percent.ToString("0.00%").Replace("," ,"."), currency.ToString("C2", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")));
 	}
 }
-
