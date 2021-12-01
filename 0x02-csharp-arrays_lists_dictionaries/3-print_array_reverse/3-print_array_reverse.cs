c135387646620e9a65d51b383977899b3204d328
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 01:20:33 -0500

    Added project 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null && array.Length > 0)
+        {
+            for (int i = array.Length - 1; i >= 1; i--)
+                Console.Write("{0} ", array[i]);
+
+            Console.WriteLine("{0}", array[0]);
+        }
+
+        else
+            Console.Write("\n");
+    }
+}
