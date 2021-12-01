005414b6d173e0ab4ba9ba9ad5dff823796a7442
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 02:01:17 -0500

    Added project 7

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index >= myList.Count || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return myList;
+        }
+
+        List<int> newList = myList;
+        newList.Remove(myList[index]);
+        return newList;
+    }
+}
