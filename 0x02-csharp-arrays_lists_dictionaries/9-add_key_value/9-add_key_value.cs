5cf8eba390153e2bda0a570f3f98ed70e9a60f64
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 10:46:37 -0500

    Added project 9

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        Dictionary<string, string> retDict = myDict;
+
+        try
+        {
+            retDict.Add(key, value);
+        }
+
+        catch (ArgumentException)
+        {
+            retDict[key] = value;
+        }
+
+        return retDict;
+    }
+}
