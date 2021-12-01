d9f77b3a23347a96400b01f11f3fe17807bfa050
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 12:01:02 -0500

    Added project 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] retMatrix = myMatrix;
+
+        for (int i = 0; i < retMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < retMatrix.GetLength(1); j++)
+            {
+                double sqrtR = Convert.ToDouble(retMatrix[i, j]);
+                int reAssgn = Convert.ToInt16(Math.Pow(sqrtR, 2));
+                retMatrix[i, j] = reAssgn;
+            }
+        }
+
+        return retMatrix;
+    }
+}
