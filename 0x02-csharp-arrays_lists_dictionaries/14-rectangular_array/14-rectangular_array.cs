aa29e268b558e02fb42419162c67b6db68d19c70
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 11:51:30 -0500

    Added project 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] multiArray = new int[5, 5];
+            
+            for (int i = 0; i < multiArray.GetLength(0); i++)
+                for (int j = 0; j < multiArray.GetLength(1); j++)
+                    multiArray[i, j] = 0;
+
+            multiArray[2, 2] = 1;
+
+            for (int i = 0; i < multiArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiArray.GetLength(1); j++)
+                    Console.Write(string.Format("{0} ", multiArray[i, j]));
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
