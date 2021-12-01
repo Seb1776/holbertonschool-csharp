4a58bca3e710e00e7dc38e98741d57d5174dc3e8
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 01:32:51 -0500

    Added project 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> newList = new List<int>();
+
+        if (size >= 0)
+        {   
+            if (size > 0)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    newList.Add(i);
+
+                    if (i == size - 1)
+                        Console.WriteLine("{0}", i);
+                    else
+                        Console.Write("{0} ", i);
+                }
+            }
+
+            else
+                Console.Write("\n");
+        }
+
+        else
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        return newList;
+    }
+}
