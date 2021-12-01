8c5d4614d4b6fcbc81a817ab24b4dac287e8c3e9
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 01:15:47 -0500

    Added project 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        int[] newArray = array;
+
+        if (index >= array.Length || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+
+        newArray[index] = n;
+        return newArray;
+    }
+}
