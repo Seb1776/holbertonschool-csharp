1c4b5d8833e7bbaad85e88d48142d4e584489289
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 11:40:27 -0500

    Added project 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        List<string> highs = myList.Keys.ToList();
+
+        if (highs.Count > 0)
+        {
+            highs.Sort();
+            return highs[highs.Count - 1];
+        }
+
+        return "None";
+    }
+}
