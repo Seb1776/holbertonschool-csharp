c994cae6237f985a27c85f8ad100f3e57068da16
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 10:52:47 -0500

    Added project 10

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        Dictionary<string, string> retDict = myDict;
+        retDict.Remove(key);
+        return retDict;
+    }
+}
+
