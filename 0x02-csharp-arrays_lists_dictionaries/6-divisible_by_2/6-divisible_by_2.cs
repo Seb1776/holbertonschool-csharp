fad81aeda6944d20442b8f670cdec67788dbf833
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 01:56:09 -0500

    Added project 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> retBool = new List<bool>();
+
+        if (myList.Count > 0)
+        {
+            List<int> divisors = new List<int> { 0, 2, 4, 6, 8 };
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (divisors.Contains(myList[i] % 10))
+                    retBool.Add(true);
+
+                else
+                    retBool.Add(false);
+            }
+        }
+
+        return retBool;
+    }
+}
