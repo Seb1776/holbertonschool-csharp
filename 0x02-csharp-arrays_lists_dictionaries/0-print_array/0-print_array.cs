223f6a954718ab67b323afed9727ece36567f187
Seb1776 <sebastianwho1776@gmail.com>
2021-12-01 01:06:24 -0500

    Added project 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size >= 0)
+        {
+            int[] retArray = new int[size];
+
+            if (size == 0)
+                Console.Write("\n");
+
+            else
+            {
+                for (int i = 0; i < size - 1; i++)
+                {
+                    retArray[i] = i;
+                    Console.Write("{0} ", i);
+                }
+                
+                retArray[size - 1] = size - 1;
+                Console.WriteLine("{0}", retArray[size - 1]);
+            }
+
+            return retArray;
+        }
+
+        Console.WriteLine("Size cannot be negative");
+        return null;    
+    }
+}
