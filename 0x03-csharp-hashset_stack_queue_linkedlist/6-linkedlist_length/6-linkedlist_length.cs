d6baea95673e5e640f85bfc186daa5723fe53a19
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 16:22:45 -0500

    Added project 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int _count = 0;
+
+        while(myLList.Last != null)
+        {
+            myLList.RemoveFirst();
+            _count++;
+        }
+
+        return _count;
+    }
+}
