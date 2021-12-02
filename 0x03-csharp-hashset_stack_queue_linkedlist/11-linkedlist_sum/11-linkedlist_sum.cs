1f209eae62a9327f181b5086923cada42e0721b7
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 17:03:03 -0500

    Added project 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        List<int> tmp = new List<int>(myLList);
+        int retSum = 0;
+
+        for (int i = 0; i < tmp.Count; i++)
+            retSum += tmp[i];
+
+        return retSum;
+    }
+}
