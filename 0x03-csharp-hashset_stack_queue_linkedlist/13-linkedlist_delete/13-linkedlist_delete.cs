795f85ae268a0b1a2ae7a690bdb51a3da602356d
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 17:19:13 -0500

    Added project 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        List<int> tmp = new List<int>(myLList);
+        int removeVal = 0;
+
+        for (int i = 0; i < tmp.Count; i++)
+        {
+            if (i == index)
+            {
+                removeVal = tmp[i];
+                break;
+            }
+        }
+
+        myLList.Remove(removeVal);
+    }
+}
