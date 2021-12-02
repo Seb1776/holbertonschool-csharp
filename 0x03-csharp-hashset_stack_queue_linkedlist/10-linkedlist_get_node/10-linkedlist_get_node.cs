3dc508991ec746e24cfb8ee845bf5e87d913bd86
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 17:00:30 -0500

    Added project 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        List<int> tmp = new List<int>(myLList);
+        int retIdx = 0;
+
+        for (int i = 0; i < tmp.Count; i++)
+            if (i == n)
+                retIdx = tmp[i];
+
+        return retIdx;
+    }
+}
