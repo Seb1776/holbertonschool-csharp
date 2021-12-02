9bef9ac461a94b1016b0776a4bf9c6c5bf6c1ba1
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 16:43:35 -0500

    Added project 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        List<int> tmp = new List<int>(myLList);
+
+        if (tmp.Contains(value))
+            for (int i = 0; i < tmp.Count; i++)
+                if (tmp[i] == value)
+                    return i;
+
+        return -1;
+    }
+}
