bb1f484fed835801864f5f451a363a37734f1630
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 14:26:06 -0500

    Added project 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> hash1 = new HashSet<int>(list1);
+        HashSet<int> hash2 = new HashSet<int>(list2);
+        hash1.SymmetricExceptWith(hash2);
+        List<int> newList = new List<int>(hash1);
+        newList.Sort();
+
+        return newList;
+    }
+}
