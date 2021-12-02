c7fd4e94df08f106374252224fd847cd0db64c53
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 16:50:05 -0500

    Added project 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int firstIdx = 0;
+
+        if (myLList.Count > 0)
+        {
+            firstIdx = myLList.First.Value;
+            myLList.RemoveFirst();
+        }
+
+        return firstIdx;
+    }
+}
