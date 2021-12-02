74dcd4f5d5474d4e199cc98f4e04cb4f658c13d3
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 16:16:30 -0500

    Added project 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> retList = new LinkedList<int>();
+
+        if (size >= 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                retList.AddFirst(i);
+                Console.WriteLine("{0}", i);
+            }
+        }
+
+        return retList;
+    }
+}
