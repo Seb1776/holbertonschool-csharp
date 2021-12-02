58a570defdab332d0692d39d80d6a3db573262ef
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 02:11:02 -0500

    Added project 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int finalRet = 0;
+        List<int> tmp = new List<int>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (!tmp.Contains(myList[i]))
+            {
+                tmp.Add(myList[i]);
+                finalRet += myList[i];
+            }
+        }
+
+        return finalRet;
+    }
+}
