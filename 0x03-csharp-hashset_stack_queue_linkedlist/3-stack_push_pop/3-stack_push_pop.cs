88c2e1371dd356e7d1cf11426485b46a00a12296
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 15:07:37 -0500

    Fixed project 3

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -8,34 +8,31 @@ class MyStack
         Console.WriteLine("Number of items: {0}", aStack.Count);
 
         if (aStack.Count > 0)
-        {
             Console.WriteLine("Top item: {0}", aStack.Peek());
-            Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
-
-            if (aStack.Contains(search))
-            {
-                int numToDelete = 0;
-
-                foreach (String str in aStack)
-                {
-                    if (str != search)
-                        numToDelete++;
-                        
-                    if (str == search)
-                    {
-                        numToDelete++;
-                        break;
-                    }
-                }
-
-                for (int i = 0; i < numToDelete; i++)
-                    aStack.Pop();
-            }
-        }
 
         else
-        {
             Console.WriteLine("Stack is empty");
+
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+
+        if (aStack.Contains(search))
+        {
+            int numToDelete = 0;
+
+            foreach (String str in aStack)
+            {
+                if (str != search)
+                    numToDelete++;
+                        
+                if (str == search)
+                {
+                    numToDelete++;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < numToDelete; i++)
+                aStack.Pop();
         }
 
         aStack.Push(newItem);
