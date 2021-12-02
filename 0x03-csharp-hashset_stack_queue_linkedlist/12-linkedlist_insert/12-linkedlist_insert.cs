88cf8a9fb56d4b30416b538d92f37ee33006d533
Seb1776 <sebastianwho1776@gmail.com>
2021-12-02 17:10:16 -0500

    Added project 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        List<int> tmp = new List<int>(myLList);
+        int insertAfterVal = 0;
+
+        for (int i = 0; i < tmp.Count; i++)
+        {
+            if (tmp[i + 1] > n)
+            {
+                insertAfterVal = tmp[i];
+                break;
+            }
+        }
+        
+        myLList.AddAfter(myLList.Find(insertAfterVal), n);
+        return myLList.Find(insertAfterVal);
+    }
+}
