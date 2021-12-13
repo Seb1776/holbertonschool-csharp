5c32679d801c7c6ef95a02ae4f64a458e4a64128
Seb1776 <sebastianwho1776@gmail.com>
2021-12-13 16:32:44 -0500

    Added 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int c = 0;
+
+        try
+        {
+            for (int i = 0; i < n; i++, c++)
+                Console.WriteLine("{0}", myList[i]);
+        }
+
+        catch(Exception){}
+
+        return c;
+    }
+}
