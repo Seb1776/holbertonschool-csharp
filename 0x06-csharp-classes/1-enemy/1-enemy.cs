7ba23a2379fbc2a08a512ea5595736c7ba7e8c44
Seb1776 <sebastianwho1776@gmail.com>
2021-12-14 15:34:17 -0500

    Fixed docu

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -2,10 +2,13 @@
 
 namespace Enemies
 {
+    ///<summary>Zombie class.</summary>
     public class Zombie
     {
+        ///<summary>Zombie health value</summary>
         public int health;
 
+        ///<summary>Zombie constructor method</summary>
         public Zombie()
         {
             health = 0;
