7589903f2fa5423dff9c8651163feafb9150eff8
Seb1776 <sebastianwho1776@gmail.com>
2021-12-13 19:48:17 -0500

    Added 3

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog (string _name, float _age, string _owner, Rating _rating)
+    {
+        name = _name;
+        age = _age;
+        owner = _owner;
+        rating = _rating;
+    }
+
+    public override string ToString()
+    {
+        string ret = string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+        return ret;
+    }
+}
