f6dbadf382231bea47eae6b96211a4409da3b21d
Seb1776 <sebastianwho1776@gmail.com>
2021-12-14 15:53:45 -0500

    Fixed docu

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -18,7 +18,7 @@ namespace Enemies
         ///<param name="value">Value that will have the health</param>
         public Zombie(int value)
         {
-            if (value <= 0)
+            if (value < 0)
                 throw new ArgumentException("Health must be greater than or equal to 0");
             
             else
