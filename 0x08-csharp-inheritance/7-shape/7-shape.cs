e49adb54382c754e761ff667a6dcca22c42ca7dc
Seb1776 <sebastianwho1776@gmail.com>
2021-12-20 17:22:38 -0500

    Fixed ToString

--- 0x08-csharp-inheritance/7-shape/7-shape.cs
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -4,6 +4,7 @@
 public class Shape
 {
     ///<summary>Method that throws a new exception</summary>
+        ///<returns>Nothing</returns>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
