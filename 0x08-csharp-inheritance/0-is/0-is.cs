45e832bd0d0a92ca2932df6bc163fecb4ca7bf90
Seb1776 <sebastianwho1776@gmail.com>
2021-12-20 16:43:30 -0500

    Added xml

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+///<summary>Obj class</summary>
 class Obj
 {
+    ///<summary>Bool method that checks if obj is an int</summary>
+        ///<param name="obj">Object to check</param>
+        ///<returns>True if its int, false if not</returns>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj is int) return true;
