3ffbe9265d829a9f39238250e401f1241393ef3a
Seb1776 <sebastianwho1776@gmail.com>
2021-12-20 17:09:38 -0500

    Added XML

--- 0x08-csharp-inheritance/8-shape/8-shape.cs
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 
+///<summary>Shape class</summary>
 public class Shape
 {
+    ///<summary>Method that throws a new exception</summary>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
+///<summary>Rectangle class, inherits from Shape</summary>
 public class Rectangle : Shape
 {
     int width, height;
 
+    ///<summary>Width constructor</summary>
     public int Width
     {
         get { return width; }
@@ -23,32 +27,38 @@ public class Rectangle : Shape
         }
     }
 
+    ///<summary>Height constructor</summary>
     public int Height
     {
         get { return height; }
 
         set 
-        { 
+        {
             if (value < 0) throw new ArgumentException("Height must be greater than or equal to 0");
             else height = value;
         }
     }
 
+    ///<summary>Method that calculates the rectangle's area</summary>
+        ///<returns>Rectangle's area</returns>
     public new int Area()
     {
         return width * height;
     }
 
+    ///<summary>Override of ToString()</summary>
     public override string ToString()
     {
         return string.Format("[Rectangle] {0} / {1}", width, height);
     }
 }
 
+///<summary>Rectangle class, inherits from Rectangle</summary>
 public class Square : Rectangle
 {
     int size;
 
+    ///<summary>Size constructor</summary>
     public int Size
     {
         get { return size; }
