fdebdca43f3feabc12cba41d5567bf5557dc5844
Seb1776 <sebastianwho1776@gmail.com>
2021-12-20 17:06:48 -0500

    Added XML

--- 0x08-csharp-inheritance/6-shape/6-shape.cs
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 
+///<summary>Shape class</summary>
 public class Shape
 {
+    ///<summary>Method that throws a new exception</summary>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
+///<summary>Rectangle class, inherits from Shape</summary>
 public class Rectangle : Shape
 {
     int width, height;
 
+    ///<summary>Width constructor</summary>
     public int Width
     {
         get { return width; }
@@ -23,12 +27,13 @@ public class Rectangle : Shape
         }
     }
 
+    ///<summary>Height constructor</summary>
     public int Height
     {
         get { return height; }
 
         set 
-        { 
+        {
             if (value < 0) throw new ArgumentException("Height must be greater than or equal to 0");
             else height = value;
         }
