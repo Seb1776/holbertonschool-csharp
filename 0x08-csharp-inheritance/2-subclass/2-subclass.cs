17467083f8c4355cbc95327b9326de267ff8cdfa
Seb1776 <sebastianwho1776@gmail.com>
2021-12-20 16:58:11 -0500

    Added XML

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
+///<summary>Obj class</summary>
 class Obj
 {
+    ///<summary>Bool method that checks if obj is a subclass</summary>
+        ///<param name="derivedType">Subclass of</param>
+        ///<param name="baseType">Main class</param>
+        ///<returns>True if its a subclass, false if not</returns>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         if (derivedType.IsSubclassOf(baseType)) return true;
