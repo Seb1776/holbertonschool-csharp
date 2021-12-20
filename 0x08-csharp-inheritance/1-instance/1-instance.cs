0067bb72d8a5bce705b71dc3c3329900f2465faf
Seb1776 <sebastianwho1776@gmail.com>
2021-12-20 16:56:19 -0500

    Added xml

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 
+///<summary>Obj class</summary>
 class Obj
 {
+    ///<summary>Bool method that checks if obj is an instance of an array</summary>
+        ///<param name="obj">Object to check</param>
+        ///<returns>True if its array, false if not</returns>
     public static bool IsInstanceOfArray(object obj)
     {
         if (obj is Array) return true;
