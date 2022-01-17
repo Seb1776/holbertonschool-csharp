df5c73c9448ea228013c36747a0d07aa0c29c9aa
Seb1776 <sebastianwho1776@gmail.com>
2022-01-17 10:46:53 -0500

    Fixed 20

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -5,7 +5,17 @@ class MatrixMath
 {
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        double[,] newMatrix = new double[,] {{-1}};
-        return (newMatrix);
+        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return (new double[,] {{-1}});
+
+        double[,] result = new double[2, 2];
+        double[,] rotate = new double[2, 2] {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}};
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int k = 0; k < 2; k++)
+                    result[i, j] = Math.Round(result[i, j] + (matrix[i, k] * rotate[k, j]), 2);
+
+        return result;
     }
 }
