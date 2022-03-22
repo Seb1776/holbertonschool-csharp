a75f1f4f56592df733096f38544fbeca54c79ab8
Seb1776 <sebastianwho1776@gmail.com>
2022-03-21 22:48:06 -0500

    Fixed queue

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -4,6 +4,7 @@
 public class Queue<T>
 {
     ///<summary>Returns the passed generic type</summary>
+        ///<returns>The string of the generic type</returns>
     public string CheckType()
     {
         return (typeof(T)).ToString();
