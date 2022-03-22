8ed7f5ad821f5d1a653dcd2e44402065e71a2e07
Seb1776 <sebastianwho1776@gmail.com>
2022-03-21 22:26:41 -0500

    Added queue

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>Generic Queue class</summary>
+public class Queue<T>
+{
+    ///<summary>Returns the passed generic type</summary>
+    public string CheckType()
+    {
+        return (typeof(T)).ToString();
+    }
+}
