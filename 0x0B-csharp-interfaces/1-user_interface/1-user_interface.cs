0005c269e189e90e3a635e9eff0cc86103aa15fd
Seb1776 <sebastianwho1776@gmail.com>
2022-03-22 12:35:33 -0500

    Added 1-user_interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+///<summary>Base class</summary>
+public abstract class Base
+{
+    ///<summary>name property</summary>
+    public string name;
+
+    ///<summary>Override of ToString method</summary>
+    public override string ToString()
+    {
+        string _ret = name + " is a " + GetType();
+        return _ret;
+  
+    }
+}
+
+///<summary>TestObject class</summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    ///<summary>Durability property</summary>
+    public int durability {get; set;}
+    ///<summary>IsCollectable property</summary>
+    public bool isCollectable {get; set;}
+
+    ///<summary>Interact method</summary>
+    public void Interact()
+    {
+
+    }
+
+    ///<summary>Break method</summary>
+    public void Break()
+    {
+
+    }
+
+    ///<summary>Collect Method</summary>
+    public void Collect()
+    {
+
+    }
+}
+
+///<summary>Interactive interface</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+///<summary>Breakable interface</summary>
+interface IBreakable
+{
+    int durability {get; set;}
+
+    void Break();
+}
+
+///<summary>Collectable interface</summary>
+interface ICollectable
+{
+    bool isCollectable {get; set;}
+
+    void Collect();
+}
