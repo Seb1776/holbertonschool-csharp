94f86440f28fb1ecac910673b0d8e543b20fc865
Seb1776 <sebastianwho1776@gmail.com>
2022-03-22 11:58:48 -0500

    Added 0-abstract_thinking

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>name property</summary>
+public abstract class Base
+{
+    ///<summary>name property</summary>
+    public string name;
+
+    ///<summary>Override of ToString method</summary>
+    public override string ToString()
+    {
+        string _ret = name + " is " + GetType();
+        return _ret;
+    }
+}
